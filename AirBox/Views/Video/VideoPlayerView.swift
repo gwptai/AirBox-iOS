87695fe01ff068b61452b1d8be777ee32ce2bb1a
gwptai <holodnoochen8@gmail.com>
2026-09-17 12:16:51 +0300

    Fix video audio playback session

--- AirBox/Views/Video/VideoPlayerView.swift
+++ AirBox/Views/Video/VideoPlayerView.swift
@@ -1,15 +1,21 @@
 import SwiftUI
 import AVKit
+import AVFoundation
 
 struct VideoPlayerView: UIViewControllerRepresentable {
 
     let url: URL
 
     func makeUIViewController(context: Context) -> AVPlayerViewController {
+        configureAudioSession()
+
         let player = AVPlayer(url: url)
         let controller = AVPlayerViewController()
         controller.player = player
+        controller.allowsPictureInPicturePlayback = true
+        controller.showsPlaybackControls = true
         controller.player?.play()
+
         return controller
     }
 
@@ -17,4 +23,15 @@ struct VideoPlayerView: UIViewControllerRepresentable {
         _ uiViewController: AVPlayerViewController,
         context: Context
     ) {}
+
+    private func configureAudioSession() {
+        let session = AVAudioSession.sharedInstance()
+
+        do {
+            try session.setCategory(.playback, mode: .moviePlayback, options: [.allowAirPlay])
+            try session.setActive(true)
+        } catch {
+            print("AirBox audio session error: \(error.localizedDescription)")
+        }
+    }
 }
